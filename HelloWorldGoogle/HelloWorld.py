584607348aa489e3bf7ca3db118bb8b6f590de3e
unknown <a.v.vishakh@gmail.com>
2018-04-24 00:20:21 +0530

    add new files to repo

---
+++ HelloWorldGoogle/HelloWorld.py
@@ -0,0 +1,14 @@
+from sklearn import tree
+import sys
+features = [[140,1], [130,1], [150,1], [170,1]]
+labels = [0,0,1,1]
+clf = tree.DecisionTreeClassifier()
+clf = clf.fit(features, labels)
+
+print(clf.predict([[150, 0]]))
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    sys.exit(int(main() or 0))
